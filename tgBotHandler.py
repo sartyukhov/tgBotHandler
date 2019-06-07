4126b5a30b32df4adc24c4908bf05a2d0561c0ef
sartyukhov <artyukhov.svyatoslav@gmail.com>
2019-06-07 15:46:15 +0300

    v 1.0.10
    - add allowed_updates
    - add static keyboard

--- tgBotHandler.py
+++ tgBotHandler.py
@@ -8,9 +8,10 @@ from urllib.parse   import quote
 from urlHandler     import urlOpener
 
 class bot:
-    def __init__(self, botID):
+    def __init__(self, botID, allowed_updates=[]):
         self.botID  = botID
         self.baseURL = 'https://api.telegram.org/bot{}/'.format(botID)
+        self.allowed_updates = allowed_updates
 
     def sendMessage(self, chatID, text, markdown=False, silent=False):
         ''' Send simple text message (with Markdown support)
@@ -27,12 +28,17 @@ class bot:
         data = '{}&text={}'.format(params, quote(text))
         return urlOpener.getUrlData(url, data=data.encode())
 
-    def getUpdate(self, offset='0', timeout='60'):
+    def getUpdate(self, offset='0', timeout='60', allowed_updates=[]):
         ''' Get updates with offset and timeout
         '''
         API    = 'getUpdates'
         url    = '{}{}?'.format(self.baseURL, API)
         params = 'offset={}&timeout={}'.format(offset, timeout)
+
+        for allow_up in (allowed_updates, self.allowed_updates):
+            if allow_up:
+                params += '&allowed_updates={}'.format(json.dumps(allow_up))
+                break
 
         if timeout == '0' or timeout == 0 or timeout is None:
             return urlOpener.getUrlData(url,\
@@ -45,7 +51,7 @@ class bot:
                 timeout=(int(timeout) * 2))
 
     def sendInKeyboard(self, chatID, text, args, columns=1, addCancel=True):
-        ''' Send keyboard to user. *args* should be tuple of tuples:
+        ''' Send inline keyboard to user. *args* should be tuple of tuples:
                         [0] : text
                         [1] : callback_data
         '''
@@ -68,6 +74,40 @@ class bot:
 
         data = '{}&text={}&reply_markup={}'\
             .format(params, quote(text), json.dumps({'inline_keyboard' : keyboard}))
+        return urlOpener.getUrlData(url, data=data.encode(), name='tg_answer')
+
+    def sendStaticKeyboard(self, chatID, text, args, columns=1, oneTime=False):
+        ''' Send static keyboard to user. *args* should be tuple of strings
+        '''
+        API    = 'sendMessage'
+        url    = '{}{}?'.format(self.baseURL, API)
+        params = '&chat_id={}'.format(chatID)
+
+        keyboard = []
+        row = []
+        for a in args:
+            row.append(a)
+            if columns <= len(row):
+                keyboard.append(row)
+                row = []
+        if 0 < len(row):
+            keyboard.append(row)
+
+        data = '{}&text={}&reply_markup={}'\
+            .format(params, 
+                quote(text), 
+                json.dumps({'keyboard' : keyboard, "one_time_keyboard":oneTime}))
+        return urlOpener.getUrlData(url, data=data.encode(), name='tg_answer')
+
+    def deleteStaticKeyboard(self, chatID, text):
+        ''' Delete static keyboard from user's interface
+        '''
+        API    = 'sendMessage'
+        url    = '{}{}?'.format(self.baseURL, API)
+        params = '&chat_id={}'.format(chatID)
+
+        data = '{}&text={}&reply_markup={}'\
+            .format(params, quote(text), json.dumps({"remove_keyboard":true}))
         return urlOpener.getUrlData(url, data=data.encode(), name='tg_answer')
 
     def deleteMessage(self, chatID, messageID):
